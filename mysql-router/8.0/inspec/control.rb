824a9b857ed208d441fe690c4d0ffe35e4e3baad
MySQL Build Team <mysql-build@oss.oracle.com>
2021-07-20 14:22:55 +0000

    Release version 1.0.4-router
      * Bump versions for 8.0.26 release

--- mysql-router/8.0/inspec/control.rb
+++ mysql-router/8.0/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '8.0.25.*' }
+    its ('version') { should match '8.0.26.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.25.*' }
+    its ('version') { should match '8.0.26.*' }
   end
 end
