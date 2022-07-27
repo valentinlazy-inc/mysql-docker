201a66c82a3aec267c0e1a848e29d23a91638711
MySQL Build Team <mysql-build@oss.oracle.com>
2022-07-27 05:32:03 +0000

    Release version 1.2.9-server
      * ET#74955: Bump versions for 8.0.30 community Docker releases

--- mysql-server/5.7/inspec/control.rb
+++ mysql-server/5.7/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-server-minimal') do
     it { should be_installed }
-    its ('version') { should match '5.7.38.*' }
+    its ('version') { should match '5.7.39.*' }
   end
   describe package('mysql-shell') do
     it { should be_installed }
-    its ('version') { should match '8.0.29.*' }
+    its ('version') { should match '8.0.30.*' }
   end
 end
