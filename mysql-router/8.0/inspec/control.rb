044356f9958bbeb9409265495035431a0e3ab8bb
MySQL Build Team <mysql-build@oss.oracle.com>
2022-10-12 04:41:17 +0000

    Release version 1.0.10-router
      * ET#75728: Bump versions for 8.0.31 community Docker releases

--- mysql-router/8.0/inspec/control.rb
+++ mysql-router/8.0/inspec/control.rb
@@ -12,10 +12,10 @@ control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '8.0.30.*' }
+    its ('version') { should match '8.0.31.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.30.*' }
+    its ('version') { should match '8.0.31.*' }
   end
 end
