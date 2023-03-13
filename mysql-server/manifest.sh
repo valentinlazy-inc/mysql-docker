e0292fb9ea329e20021c9392f3d7722b580d4c54
Robert Neumayer <robert.neumayer@oracle.com>
2023-03-13 13:01:26 +0100

    Use amd64 and arm64 as architecture names
    Change-Id: I6c01255fac72990d02fad2ed5e797a9e0bd84049

--- mysql-server/manifest.sh
+++ mysql-server/manifest.sh
@@ -18,7 +18,7 @@ source ./VERSION
 
 if grep -q Microsoft /proc/version; then
   echo "Running on Windows Subsystem for Linux"
-  # WSL doesn't have its own docker host, we have to use the one 
+  # WSL doesn't have its own docker host, we have to use the one
   # from Windows itself.
   # https://medium.com/@sebagomez/installing-the-docker-client-on-ubuntus-windows-subsystem-for-linux-612b392a44c4
   export DOCKER_HOST=localhost:2375
@@ -29,8 +29,8 @@ REPO=mysql/mysql-server; [ -n "$1" ] && REPO=$1
 for MAJOR_VERSION in ${MULTIARCH_VERSIONS}; do
   MANIFEST_VERSIONS=$(./tag.sh "" "$MAJOR_VERSION")
   for MANIFEST_VERSION in $MANIFEST_VERSIONS; do
-    docker manifest create "$REPO:$MANIFEST_VERSION" "$REPO:$MANIFEST_VERSION-aarch64" "$REPO:$MANIFEST_VERSION-amd64"
-    docker manifest annotate "$REPO:$MANIFEST_VERSION" "$REPO:$MANIFEST_VERSION-aarch64" --os linux --arch arm64
+    docker manifest create "$REPO:$MANIFEST_VERSION" "$REPO:$MANIFEST_VERSION-arm64" "$REPO:$MANIFEST_VERSION-amd64"
+    docker manifest annotate "$REPO:$MANIFEST_VERSION" "$REPO:$MANIFEST_VERSION-arm64" --os linux --arch arm64
     docker manifest annotate "$REPO:$MANIFEST_VERSION" "$REPO:$MANIFEST_VERSION-amd64" --os linux --arch amd64
     docker manifest push "$REPO:$MANIFEST_VERSION"
   done
