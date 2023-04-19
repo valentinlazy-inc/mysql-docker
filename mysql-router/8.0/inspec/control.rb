4f52c7f0bd3c49094e062a778ec04e8cf284059b
MySQL Build Team <mysql-build@oss.oracle.com>
2023-04-19 08:55:43 +0000

    Release version 1.0.12-router
      * [Docker] - Router - tag-release pipeline - Fix the MAJOR_VERSION usage
      * Revert "Fix for ET#77620, 77619, 77611, 70570"
      * Fix for ET#77620, 77619, 77611, 70570
      * ET#70570 - [docker] setup weekly router docker image builds
      * Router: Test for the correct command
      * Switch to arm64 for arm tags
      * Router: Update tests for podman
      * Update image tests to work with podman
      * ET#77361: Bump versions for 8.0.33 Docker releases
      * ET#77271 - [Docker] Restructure internal-enterprise - router pipeline
      * ET#77004 - [WIP] Create Docker ARM image for MySQL Router - Follow up fix - updated permission for manifest file
      * ET#77004 - [WIP] Create Docker ARM image for MySQL Router
      * [Docker] Updating the copyright year in the header
      * ET77115 - [Docker] Use OCR for fetching base images for docker builds

--- mysql-router/8.0/inspec/control.rb
+++ mysql-router/8.0/inspec/control.rb
@@ -1,21 +1,20 @@
 control 'container' do
   impact 0.5
-  describe docker_container('mysql-router-8.0') do
-    it { should exist }
-    it { should be_running }
-    its('repo') { should eq 'mysql/mysql-router' }
-    its('ports') { should eq '6446-6449/tcp, 8443/tcp' }
-    its('command') { should match '/run.sh.*' }
+  describe podman.containers do
+    its('status') { should cmp /Up/ }
+    its('commands') { should cmp /sleep/ }
+    its('images') { should cmp /mysql-router:8.0/ }
+    its('names') { should include "mysql-router-8.0" }
   end
 end
 control 'packages' do
   impact 0.5
   describe package('mysql-community-client') do
     it { should be_installed }
-    its ('version') { should match '8.0.32.*' }
+    its ('version') { should match '8.0.33.*' }
   end
   describe package('mysql-router-community') do
     it { should be_installed }
-    its ('version') { should match '8.0.32.*' }
+    its ('version') { should match '8.0.33.*' }
   end
 end
